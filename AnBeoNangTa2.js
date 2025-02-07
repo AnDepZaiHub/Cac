ef317146f7971a17bba7f07e3183577f6ca2c831
AnDepZaiHub <xuanphung0808@gmail.com>
2025-02-07 19:45:44 +0700

    Update AnBeoNangTa2.js

--- AnBeoNangTa2.js
+++ AnBeoNangTa2.js
@@ -75,7 +75,7 @@ app.get('/api/darkbear', async (req, res) => {
 
 client.login(TOKEN);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
